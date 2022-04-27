70bdac9ae92294b53c94518b77b4ba00b9947681
Diego Melo <diegomelo182@gmail.com>
2022-04-27 01:34:41 +0100

    feat: title menu

--- shared/components/topbar/topbar.js
+++ shared/components/topbar/topbar.js
@@ -10,7 +10,7 @@ export default function TopBar() {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Farfetch
+            SpaceX Launches
           </Typography>
         </Toolbar>
       </AppBar>
